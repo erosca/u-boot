f0a558ee360836af43b6c7e39b7d3ec5450247e8
Eugeniu Rosca <erosca@de.adit-jv.com>
2019-12-12 13:26:22 +0100

    cmd: adtimg: Refactor usage style
    Trying to extend 'adtimg' functionality [1], we've been severely hit
    by a major limitation in the command's usage scheme. Specifically, the
    command's user interface appears to be too centric to getting the
    DTB/DTBO entry [3] based on the index of the desired DT in the image,
    which makes it really difficult retrieving the DT entry based on
    alternative criteria (e.g. filtering by id/rev/custom fields), the
    latter being demanded by real life customer use-cases [1].
    
    This went to the point of receiving below feedback from Sam [2]:
    
     -- snip --
     As for 'dtimg' command: after giving it some thought, I think not much
     people using it yet. So in this particular case I don't have some
     strong preference, and if you think the 'dtimg' interface is ugly, and
     it overcomes "don't break interfaces" rule, maybe now is a good time
     to rework it (before it gets widely used).
     -- snip --
    
    Given the above, rework the usage pattern from [4] to [5], in order to
    allow an intuitive enablement of "by id|rev|custom" DT search [6].
    
    [1] https://patchwork.ozlabs.org/cover/1202575/
        ("cmd: dtimg: Enhance with --id and --rev options (take #1)")
    [2] https://patchwork.ozlabs.org/patch/1182207/#2317020
    [3] https://source.android.com/devices/architecture/dto/partitions
    [4] Old usage
    adtimg dump <addr>                    - Print image contents
    adtimg start <addr> <index> <varname> - Get DT address by index
    adtimg size <addr> <index> <varname>  - Get DT size by index
    
    [5] New usage
    adtimg addr <addr>                      - Set image location to <addr>
    adtimg dump                             - Print out image contents
    adtimg get dt index <index> [avar svar] - Get DT address and size by index
    
    [6] Expected/upcoming "by id|rev|custom" add-on functionality
    adtimg get dt by <id> <rev> <custom> [avar svar]
     - Get DT address and size by id|rev|custom fields
    
    Cc: Sam Protsenko <semen.protsenko@linaro.org>
    Signed-off-by: Eugeniu Rosca <erosca@de.adit-jv.com>

--- cmd/adtimg.c
+++ cmd/adtimg.c
@@ -2,18 +2,20 @@
 /*
  * (C) Copyright 2018 Linaro Ltd.
  * Sam Protsenko <semen.protsenko@linaro.org>
+ * Eugeniu Rosca <rosca.eugeniu@gmail.com>
  */
 
 #include <env.h>
 #include <image-android-dt.h>
 #include <common.h>
 
-enum cmd_adtimg_info {
-	CMD_ADTIMG_START = 0,
-	CMD_ADTIMG_SIZE,
-};
+/*
+ * Current/working DTB/DTBO Android image address.
+ * Similar to 'working_fdt' variable in 'fdt' command.
+ */
+static ulong working_img;
 
-static int do_adtimg_dump(cmd_tbl_t *cmdtp, int flag, int argc,
+static int do_adtimg_addr(cmd_tbl_t *cmdtp, int flag, int argc,
 			  char * const argv[])
 {
 	char *endp;
@@ -24,86 +26,131 @@ static int do_adtimg_dump(cmd_tbl_t *cmdtp, int flag, int argc,
 
 	hdr_addr = simple_strtoul(argv[1], &endp, 16);
 	if (*endp != '\0') {
-		printf("Error: Wrong image address\n");
+		printf("Error: Wrong image address '%s'\n", argv[1]);
 		return CMD_RET_FAILURE;
 	}
 
-	if (!android_dt_check_header(hdr_addr)) {
-		printf("Error: DT image header is incorrect\n");
+	/*
+	 * Allow users to set an address prior to copying the DTB/DTBO
+	 * image to that same address, i.e. skip header verification.
+	 */
+
+	working_img = hdr_addr;
+	return CMD_RET_SUCCESS;
+}
+
+static int adtimg_check_working_img(void)
+{
+	if (!working_img) {
+		puts("Error: Please, call 'adtimg addr <addr>'. Aborting!\n");
 		return CMD_RET_FAILURE;
 	}
 
-	android_dt_print_contents(hdr_addr);
+	if (!android_dt_check_header(working_img)) {
+		printf("Error: Invalid image header at 0x%lx\n", working_img);
+		return CMD_RET_FAILURE;
+	}
 
 	return CMD_RET_SUCCESS;
 }
 
-static int adtimg_get_fdt(int argc, char * const argv[],
-			  enum cmd_adtimg_info cmd)
-{
-	ulong hdr_addr;
-	u32 index;
-	char *endp;
-	ulong fdt_addr;
-	u32 fdt_size;
-	char buf[65];
-
-	if (argc != 4)
-		return CMD_RET_USAGE;
-
-	hdr_addr = simple_strtoul(argv[1], &endp, 16);
-	if (*endp != '\0') {
-		printf("Error: Wrong image address\n");
-		return CMD_RET_FAILURE;
-	}
-
-	if (!android_dt_check_header(hdr_addr)) {
-		printf("Error: DT image header is incorrect\n");
-		return CMD_RET_FAILURE;
-	}
-
-	index = simple_strtoul(argv[2], &endp, 0);
-	if (*endp != '\0') {
-		printf("Error: Wrong index\n");
-		return CMD_RET_FAILURE;
-	}
-
-	if (!android_dt_get_fdt_by_index(hdr_addr, index, &fdt_addr, &fdt_size))
-		return CMD_RET_FAILURE;
-
-	switch (cmd) {
-	case CMD_ADTIMG_START:
-		snprintf(buf, sizeof(buf), "%lx", fdt_addr);
-		break;
-	case CMD_ADTIMG_SIZE:
-		snprintf(buf, sizeof(buf), "%x", fdt_size);
-		break;
-	default:
-		printf("Error: Unknown cmd_adtimg_info value: %d\n", cmd);
-		return CMD_RET_FAILURE;
-	}
-
-	env_set(argv[3], buf);
-
-	return CMD_RET_SUCCESS;
-}
-
-static int do_adtimg_start(cmd_tbl_t *cmdtp, int flag, int argc,
-			   char * const argv[])
-{
-	return adtimg_get_fdt(argc, argv, CMD_ADTIMG_START);
-}
-
-static int do_adtimg_size(cmd_tbl_t *cmdtp, int flag, int argc,
+static int do_adtimg_dump(cmd_tbl_t *cmdtp, int flag, int argc,
 			  char * const argv[])
 {
-	return adtimg_get_fdt(argc, argv, CMD_ADTIMG_SIZE);
+	if (argc != 1)
+		return CMD_RET_USAGE;
+
+	if (adtimg_check_working_img() != CMD_RET_SUCCESS)
+		return CMD_RET_FAILURE;
+
+	android_dt_print_contents(working_img);
+
+	return CMD_RET_SUCCESS;
+}
+
+static int adtimg_get_dt_by_index(int argc, char * const argv[])
+{
+	ulong addr;
+	u32 size, index;
+	char *endp;
+
+	if (argc < 1) {
+		puts("Error: Option 'get dt index' requires an argument\n");
+		return CMD_RET_FAILURE;
+	}
+
+	if (argc > 3) {
+		puts("Error: Too many arguments for 'get dt index'\n");
+		return CMD_RET_FAILURE;
+	}
+
+	index = simple_strtoul(argv[0], &endp, 0);
+	if (*endp != '\0') {
+		printf("Error: Wrong index value '%s'\n", argv[0]);
+		return CMD_RET_FAILURE;
+	}
+
+	if (!android_dt_get_fdt_by_index(working_img, index, &addr, &size))
+		return CMD_RET_FAILURE;
+
+	if (argc == 3) {
+		env_set_hex(argv[1], addr);
+		env_set_hex(argv[2], size);
+	} else {
+		printf("0x%lx, 0x%x (dec:%d)\n", addr, size, size);
+	}
+
+	return CMD_RET_SUCCESS;
+}
+
+static int adtimg_get_dt(int argc, char * const argv[])
+{
+	const char *param;
+
+	if (argc < 1) {
+		puts("Error: Option 'get dt' requires an argument\n");
+		return CMD_RET_FAILURE;
+	}
+
+	param = argv[0];
+	argc--;
+	argv++;
+
+	if (!strcmp(param, "index"))
+		return adtimg_get_dt_by_index(argc, argv);
+
+	printf("Error: Wrong option 'get dt %s'\n", param);
+	return CMD_RET_FAILURE;
+}
+
+static int do_adtimg_get(cmd_tbl_t *cmdtp, int flag, int argc,
+			 char * const argv[])
+{
+	const char *param;
+
+	if (argc < 2) {
+		puts("Error: Option 'get' requires an argument\n");
+		return CMD_RET_FAILURE;
+	}
+
+	if (adtimg_check_working_img() != CMD_RET_SUCCESS)
+		return CMD_RET_FAILURE;
+
+	param = argv[1];
+	argc -= 2;
+	argv += 2;
+
+	if (!strcmp(param, "dt"))
+		return adtimg_get_dt(argc, argv);
+
+	printf("Error: Wrong option 'get %s'\n", param);
+	return CMD_RET_FAILURE;
 }
 
 static cmd_tbl_t cmd_adtimg_sub[] = {
-	U_BOOT_CMD_MKENT(dump, 2, 0, do_adtimg_dump, "", ""),
-	U_BOOT_CMD_MKENT(start, 4, 0, do_adtimg_start, "", ""),
-	U_BOOT_CMD_MKENT(size, 4, 0, do_adtimg_size, "", ""),
+	U_BOOT_CMD_MKENT(addr, CONFIG_SYS_MAXARGS, 1, do_adtimg_addr, "", ""),
+	U_BOOT_CMD_MKENT(dump, CONFIG_SYS_MAXARGS, 1, do_adtimg_dump, "", ""),
+	U_BOOT_CMD_MKENT(get, CONFIG_SYS_MAXARGS, 1, do_adtimg_get, "", ""),
 };
 
 static int do_adtimg(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
@@ -127,17 +174,13 @@ static int do_adtimg(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 U_BOOT_CMD(
 	adtimg, CONFIG_SYS_MAXARGS, 0, do_adtimg,
 	"manipulate dtb/dtbo Android image",
-	"dump <addr>\n"
-	"    - parse specified image and print its structure info\n"
-	"      <addr>: image address in RAM, in hex\n"
-	"adtimg start <addr> <index> <varname>\n"
-	"    - get address (hex) of FDT in the image, by index\n"
-	"      <addr>: image address in RAM, in hex\n"
-	"      <index>: index of desired FDT in the image\n"
-	"      <varname>: name of variable where to store address of FDT\n"
-	"adtimg size <addr> <index> <varname>\n"
-	"    - get size (hex, bytes) of FDT in the image, by index\n"
-	"      <addr>: image address in RAM, in hex\n"
-	"      <index>: index of desired FDT in the image\n"
-	"      <varname>: name of variable where to store size of FDT"
+	"addr <addr>\t\t\t- Set image location to <addr>\n"
+	"adtimg dump\t\t\t\t- Print out image contents\n"
+	"adtimg get dt index <index> [avar svar]\t- Get DT address and size by index\n"
+	"\n"
+	"Legend:\n"
+	"  - <addr>: DTB/DTBO image address (hex) in RAM\n"
+	"  - <index>: index (hex/dec) of desired DT in the image\n"
+	"  - <avar>: variable name to contain DT address (hex)\n"
+	"  - <svar>: variable name to contain DT size (hex)"
 );
